db8f9697392597626c4a5cea8492bde44a9b3887
grace <calianese.g@northeastern.edu>
2023-06-27 14:40:20 -0400

    added String dir field to the VERTICAL and HORIZONTAL enums, added a flip method to go back and forth between them

--- src/main/java/cs3500/pa05/model/Direction.java
+++ src/main/java/cs3500/pa05/model/Direction.java
@@ -4,6 +4,19 @@ package cs3500.pa05.model;
  * represents directions for a GUI layout
  */
 public enum Direction {
-  HORIZONTAL,
-  VERTICAL
+  HORIZONTAL("horizontal"),
+  VERTICAL("vertical");
+
+  private String dir;
+  Direction(String dir) {
+    this.dir = dir;
+  }
+
+  public static String flip(String d) {
+    if (d.equals(HORIZONTAL.dir)) {
+      return VERTICAL.dir;
+    } else {
+      return HORIZONTAL.dir;
+    }
+  }
 }
